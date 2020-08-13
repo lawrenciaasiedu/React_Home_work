baabb49b8e8f05099d3464bfb05e2cc457a143b5
gregaid427 <gregoryd427@gmail.com>
2020-08-12 21:38:49 -1200

    Create Navbar.js

---
+++ src/Component/Navbar.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+
+
+
+
+class Navbar  extends Component {
+    render(){
+        return(    
+        <div>
+         
+    <nav className="navbar">
+      <div className="container">
+        <a href="hulk">Hulk</a>
+        
+        <a href="hulk" className="logo is-active">Mavel</a>
+        <a href="hulk">Venom</a>
+      </div>
+    </nav>
+         </div> 
+           
+        
+        )
+    }
+    
+}
+export default Navbar
